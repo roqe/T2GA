469717e4ba7ef705f6c6fb0cf2716029933586b9
roqe <octopusjune@gmail.com>
2016-12-14 20:57:35 +0800

    modified examples

--- R/computeT2.R
+++ R/computeT2.R
@@ -15,9 +15,9 @@
 #' @importFrom parallel mclapply
 #' @examples
 #' dat1=importdata(tcr_05)
-#' dat2=importdata(tcr_05,tcr_15)
 #' res1=computeT2(dat1)
-#' res2=computeT2(dat2,vex=Reactome_vex,ppi=HitPredict_ppi)
+#' dat2=importdata(fileName1=tcr_05,fileName2=tcr_15)
+#' res2=computeT2(dat2,pathDB="Reactome",ppi=HitPredict_ppi)
 
 computeT2=function(data,purb=1.5,pathDB="KEGG",ppi=STRING_ppi,intg=TRUE,alpha=0.05,ncore=1,per=10000){
   if(pathDB=="Reactome"){
